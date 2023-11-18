0f293e228d21cf716a9341aa95e3179fad6734a4
fjborrell <83686662+fjborrell@users.noreply.github.com>
2023-11-18 17:52:23 -0500

    Initial commit w/ Xcode project

---
+++ Scooter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scooter
+//
+//  Created by Fernando Borrell on 11/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scooter/ScooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScooterApp.swift
+//  Scooter
+//
+//  Created by Fernando Borrell on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ScooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
